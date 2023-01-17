27386cd2240663dfe9a6c52f79d770e5e8da7ac8
Bora <alclsloo@naver.com>
2023-01-17 17:16:27 +0900

    변경사항 커밋22

---
+++ src/Step2/boj18108.java
@@ -0,0 +1,15 @@
+package Step2;
+
+import java.util.Scanner;
+
+public class boj18108 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+
+        System.out.println(A - 543);
+
+        sc.close();
+    }
+}
